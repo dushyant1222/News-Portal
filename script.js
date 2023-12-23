8d93a16274f5d8b181a931fbd3deb6fe487f4a7e
Dushyant Kumar <149196558+dushyant1222@users.noreply.github.com>
2023-12-23 11:35:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("searchButton").addEventListener("click", function() {
+    // Get the value from the search input
+    var searchTerm = document.getElementById("searchInput").value;
+  
+    // Perform the search or any other action based on the search term
+    console.log("Search term:", searchTerm);
+    // Add your search functionality or redirect to a search results page here
+  });
+  
